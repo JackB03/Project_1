67d33bc8d15f3e43c4c3fc7b2d6f54edd11d1069
JackBonner <jackb03@fullsail.edu>
2015-10-03 10:44:49 -0500

    Added Employees to Array List - Verified with Log.i

--- app/src/main/java/android/fullsail/com/project_1/MainActivity.java
+++ app/src/main/java/android/fullsail/com/project_1/MainActivity.java
@@ -2,8 +2,11 @@ package android.fullsail.com.project_1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +15,27 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        String TAG = "Employee Project";
 
-        //Array List
+        // Employees
+        Employee president = new Employee("Jack", "Bonner");
+        Employee vicePresident = new Employee("Kenn", "Williams");
+        Employee socialManager = new Employee("Britney", "Elbert");
+        Employee graphicDesigner = new Employee("Rick","Wallace");
+        Employee webDeveloper = new Employee("Elena","Martinez");
+
+        //Add to Array List for Employees
+        ArrayList<Employee> employeeArray = new ArrayList<>();
+        employeeArray.add(president);
+        employeeArray.add(vicePresident);
+        employeeArray.add(socialManager);
+        employeeArray.add(graphicDesigner);
+        employeeArray.add(webDeveloper);
+
+        for(Employee employees : employeeArray){
+            Log.i(TAG, employees.getFirstName() + " " + employees.getLastName());
+        }
+
 
 
 
